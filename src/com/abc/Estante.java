541137fb10c3a2d41bb039092ae7687a89a908d3
IgnacioPeralta00 <00196321@uca.edu.sv>
2025-04-22 17:42:49 -0600

    add: Estante.java class

---
+++ src/com/abc/Estante.java
@@ -0,0 +1,12 @@
+package com.abc;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Estante {
+    private List<Libro> libros;
+
+    public Estante() {
+        this.libros = new ArrayList<>();
+    }
+}
